d3d5ca0eaab638f066fabe28da5ecab3313aa38a
rishi jung <ranabhat.85@gmail.com>
2022-05-22 21:14:24 +0545

    add lambda handler to upload csv files

--- step_function/csv_uploader.py
+++ step_function/csv_uploader.py
@@ -1,5 +1,6 @@
 import json
-from requests_toolbelt.multipart import decoder
+from requests_toolbelt import MultipartDecoder
+import base64
 
 def make_response_obj(body, status_code=200):
     return {
@@ -12,7 +13,34 @@ def make_response_obj(body, status_code=200):
         })
     }
 
+FILE_HEADERS = {
+    "first_name": {type: "str"},
+    "last_name": {type: "str"},
+    "domain": {type: "str"},
+    "company": {type: "str"},
+}
+
 def lambda_handler(event, context):
-    json_body = event['body-json']
-    data = json_body.split("\n")
-    return make_response_obj(f'Hello from Lambda!{data}')
+    body = event['body']
+
+    content_type = event['headers']['Content-Type']
+    content = base64.b64decode(body)
+
+    decoder = MultipartDecoder(content, content_type)
+    
+    rows = []
+    headers = []
+    for i, part in enumerate(decoder.parts):
+        if i == 0:
+            headers = part.text.split(",")
+            for j in headers:
+                if j.lower() not in list(FILE_HEADERS.values()):
+                    return make_response_obj("Invalid file headers", 400)
+        else:
+            row = part.text.split(",")
+            rows.append(row)
+
+    if len(rows) < 10:
+        return make_response_obj("records should greater or equal to 10", 400)
+
+    return make_response_obj(f'Hello from Lambda! {rows}')
